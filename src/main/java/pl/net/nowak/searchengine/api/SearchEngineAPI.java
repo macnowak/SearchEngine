0ee6648e7601a8f736e47218ef6d75926bc727ad
maciek <maku17@gmail.com>
2015-04-27 20:11:51 +0200

    working spring + h2

--- src/main/java/pl/net/nowak/searchengine/api/SearchEngineAPI.java
+++ src/main/java/pl/net/nowak/searchengine/api/SearchEngineAPI.java
@@ -1,8 +1,11 @@
 package pl.net.nowak.searchengine.api;
 
+import pl.net.nowak.core.annotations.API;
+
 /**
  * Created by mno on 2015-04-27.
  */
+@API
 public class SearchEngineAPI {
 
     public String performQuery(String query, String filter) {
